7a5d60834fb6cece3d64a8d6370e21ae4e0ba013
Robert S <rsavian@nerdery.com>
2019-12-23 08:36:01 -0600

    add url

--- src/environments/base.ts
+++ src/environments/base.ts
@@ -1,6 +1,7 @@
 /*
  * Base is the default environment for production.
  * Add everything here and override value in other files if needed.
+ * https://blog.usejournal.com/my-awesome-custom-react-environment-variables-setup-8ebb0797d8ac
  */
 export default function baseEnv(baseApi: string) {
   return {
