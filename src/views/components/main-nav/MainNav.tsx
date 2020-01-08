0241181089d485df125fbbf856bf65cdf1365f47
Robert S <rsavian@nerdery.com>
2020-01-08 09:31:11 -0600

    add NavSearch

--- src/views/components/main-nav/MainNav.tsx
+++ src/views/components/main-nav/MainNav.tsx
@@ -1,32 +1,18 @@
 import styles from './MainNav.module.scss';
 
-import React, { useCallback, useState } from 'react';
-import { Menu, Segment, Form, InputOnChangeData, FormProps } from 'semantic-ui-react';
+import React from 'react';
+import { Menu, Segment } from 'semantic-ui-react';
 import MenuNavLink from './components/MenuNavLink';
 import RouteEnum from '../../../constants/RouteEnum';
 import { observer } from 'mobx-react';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import NavSearch from './components/nav-search/NavSearch';
 interface IProps {}
 
 const MainNav: React.FC<IProps> = observer((props) => {
   const location = useLocation();
-  const history = useHistory();
-
-  const [searchTerm, setSearchTerm] = useState('');
   const { pathname } = location;
   const hideSearchInput = pathname !== RouteEnum.Search;
-
-  const onChangeInput = useCallback((event: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => {
-    setSearchTerm(data.value);
-  }, []);
-  const onClickSearch = useCallback(
-    (event: React.FormEvent<HTMLFormElement>, data: FormProps) => {
-      history.push(`${RouteEnum.Search}?term=${searchTerm}`);
-
-      setSearchTerm('');
-    },
-    [history, searchTerm]
-  );
 
   return (
     <Segment inverted={true} className={styles.wrapper}>
@@ -35,17 +21,7 @@ const MainNav: React.FC<IProps> = observer((props) => {
         <Menu.Item as={MenuNavLink} to={RouteEnum.Episodes} name="Episodes" />
         <Menu.Item as={MenuNavLink} to={RouteEnum.About} name="About" />
       </Menu>
-      {hideSearchInput && (
-        <Form onSubmit={onClickSearch}>
-          <Form.Input
-            name="searchTerm"
-            icon={{ name: 'search', icon: 'search' }}
-            placeholder="Search for Shows..."
-            value={searchTerm}
-            onChange={onChangeInput}
-          />
-        </Form>
-      )}
+      {hideSearchInput && <NavSearch />}
     </Segment>
   );
 });
